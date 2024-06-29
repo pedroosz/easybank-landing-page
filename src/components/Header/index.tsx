7b8da578cd760888bb832a7c76aa114dbd561199
Pedro Souza <pedrodinp.70@gmail.com>
2024-06-28 21:06:13 -0300

    :recycle: creates a navlink component and adds mobile menu hamburguer

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -2,8 +2,16 @@
 
 import useHeaderStore from "@/stores/header-store";
 import Image from "next/image";
-import Link from "next/link";
+import Link, { LinkProps } from "next/link";
+import { PropsWithChildren } from "react";
+import { FaBars } from "react-icons/fa6";
 import CtaLink from "../cta-link";
+
+interface NavLinkProps extends LinkProps, PropsWithChildren {}
+
+function NavLink(props: NavLinkProps) {
+  return <Link className="duration-200 hover:text-black" {...props} />;
+}
 
 export default function Header() {
   const { toggle, isOpen } = useHeaderStore();
@@ -11,18 +19,18 @@ export default function Header() {
   return (
     <header
       data-mobile-toggle={isOpen}
-      className="relative z-10 flex h-20 w-full items-center bg-white px-4"
+      className="fixed top-0 z-10 flex h-20 w-full items-center bg-white px-4"
     >
       {/* Desktop header */}
       <div className="mx-auto hidden w-full max-w-container items-center justify-between gap-4 lg:flex">
         <Image src="/logo.svg" alt="EasyBank" width={120} height={30} />
 
         <nav className="flex items-center gap-4 text-sm">
-          <Link href="#">Home</Link>
-          <Link href="#">About</Link>
-          <Link href="#">Contact</Link>
-          <Link href="#">Blog</Link>
-          <Link href="#">Careers</Link>
+          <NavLink href="#">Home</NavLink>
+          <NavLink href="#">About</NavLink>
+          <NavLink href="#">Contact</NavLink>
+          <NavLink href="#">Blog</NavLink>
+          <NavLink href="#">Careers</NavLink>
         </nav>
 
         <CtaLink href="#">Request Invite</CtaLink>
@@ -31,16 +39,18 @@ export default function Header() {
       {/* Mobile header */}
       <div className="relative mx-auto flex w-full max-w-container items-center justify-between gap-4 lg:hidden">
         <Image src="/logo.svg" alt="EasyBank" width={120} height={30} />
-        <button onClick={toggle}>toggle</button>
+        <button onClick={toggle} aria-label="Navigation menu toggle">
+          <FaBars />
+        </button>
 
         {/* Mobile menu dropdown */}
         {isOpen && (
           <div className="absolute top-full mt-10 flex w-full flex-col items-center justify-center gap-4 rounded-md bg-white p-4 text-center shadow-2xl shadow-dark-blue">
-            <Link href="#">Home</Link>
-            <Link href="#">About</Link>
-            <Link href="#">Contact</Link>
-            <Link href="#">Blog</Link>
-            <Link href="#">Careers</Link>
+            <NavLink href="#">Home</NavLink>
+            <NavLink href="#">About</NavLink>
+            <NavLink href="#">Contact</NavLink>
+            <NavLink href="#">Blog</NavLink>
+            <NavLink href="#">Careers</NavLink>
           </div>
         )}
       </div>
